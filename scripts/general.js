2feb55fcfb4c0b27a0e1300a1b4cb877984e16ab
ShadyBarrios <scottpsn16@gmail.com>
2025-11-01 05:00:02 -0700

    Devo Kilroy and UI Update

--- scripts/general.js
+++ scripts/general.js
@@ -3,5 +3,35 @@ function viewPDF(edition){
     if(edition in editions)
         window.open("zines/" + editions[edition], "_blank");
     else
-        alert("Invalid Zine Index")
+        alert("Invalid Zine Index");
+}
+
+// all audios
+const audioIDs = ["WhipItAudio"]
+// all audio playing statuses
+var audioPlaying = {
+    WhipItAudio: false
+};
+// when an audio is clicked
+function toggleAudio(audioID){
+    console.log("here")
+    var audioPlayer = document.getElementById(audioID);
+    // if audio is already playing, then pause it
+    if(audioPlaying[audioID]){
+        audioPlayer.pause();
+        audioPlaying[audioID] = false;
+    }
+    else{ // if audio is not playing (yet), then pause all other audios and then play this one
+        pauseAllAudios();
+        audioPlayer.play();
+        audioPlaying[audioID] = true;
+    }
+}
+// pauses all audios
+function pauseAllAudios(){
+    var audioPlayer;
+    for(const audioID of audioIDs){
+        audioPlayer = document.getElementById(audioID);
+        audioPlayer.pause();
+    }
 }
